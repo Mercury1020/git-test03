2219f85a91d777db3165c358a76d91b36ace0ddc
QiuXiaoLi <QiuXiaoLi@atguigu.com>
2023-02-15 15:38:06 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author Yolo1020
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git2!");
+    }
+}
